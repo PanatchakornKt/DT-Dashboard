0601ac506720eb3842f5aa9908101a82c8da981f
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-05-24 12:44:05 +0700

    create button bar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 const MainLayout = ({ children }) => {
+
   return (
-    <div
-      className="min-h-screen bg-gray-200"
-      onSubmit={(e) => e.preventDefault()}
-    >
+    <div className="min-h-screen bg-gray-200">
+
       <div className="w-100 max-w-4xl mx-auto p-5">
         <h1 className="text-4xl font-bold undefined">Daytech Dashboard</h1>
         <div className="my-5">
