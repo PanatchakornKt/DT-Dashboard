a67aa0972981875c71be2f1798066c08e0c14f9f
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-05-24 19:46:03 +0700

    change nevbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
+import Link from "next/link";
 import AddWidgets from "../components/addWidgets";
-
 import CardDefault from "../components/CardDefault";
 import CardJustSay from "../components/CardJustsay";
 import CardCounter from "../components/CardCounter";
@@ -10,6 +10,7 @@ const index = () => {
   return (
     <>
       <title>Widgets - Daytech Dashbroad</title>
+      <h2 className="text-xl undefined">Widgets</h2>
       <div className="pt-3">
         <AddWidgets />
         <div className="md:flex md:flex-wrap md:mr-4">
