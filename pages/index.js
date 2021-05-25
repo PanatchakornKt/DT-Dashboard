7f39a21525ff6d64b9c1c892b0b515144360b8e9
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-05-25 16:24:57 +0700

    add widgets timer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Link from "next/link";
 import AddWidgets from "../components/addWidgets";
-import CardDefault from "../components/CardDefault";
-import CardJustSay from "../components/CardJustsay";
-import CardCounter from "../components/CardCounter";
-import CardTimer from "../components/CardTimer";
+// import CardDefault from "../components/CardDefault";
+// import CardJustSay from "../components/CardJustsay";
+// import CardCounter from "../components/CardCounter";
+// import CardTimer from "../components/CardTimer";
 
 const index = () => {
   return (
@@ -13,12 +13,6 @@ const index = () => {
       <h2 className="text-xl undefined">Widgets</h2>
       <div className="pt-3">
         <AddWidgets />
-        <div className="md:flex md:flex-wrap md:mr-4">
-          <CardDefault />
-          <CardJustSay />
-          <CardTimer />
-          <CardCounter />
-        </div>
       </div>
     </>
   );
