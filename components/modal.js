0318b200f3b67dbc9f609de664989395dffbca07
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-06-01 14:05:39 +0700

    edit madal file

--- components/modal.js
+++ components/modal.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { IoClose } from "react-icons/io5";
+import { IoClose } from 'react-icons/io5';
 
 const Modal = ({ children, onCancel = () => {} }) => {
   return (
-    <>
-    <div className="fixed flex items-center justify-center top-0 right-0 bottom-0 left-0 bg-black bg-opacity-70 tailwind class name z-50 ">
-      <div className="relative bg-gray-200 m-5 p-6 pt-4 md:p-8 md:pt-6 rounded-2xl w-96 max-w-full">
-        <p></p>
+    <div className='fixed flex items-center py-5 justify-center top-0 right-0 bottom-0 left-0 bg-black bg-opacity-70 z-50'>
+      <div className='relative bg-gray-200 m-5 p-6 pt-4 md:p-8 md:pt-6 rounded-2xl w-96 max-w-full max-h-full overflow-auto'>
         <button
-          className="absolute text-lg text-gray-600 top-4 right-4 focus:outline-none"
+          className='absolute text-lg text-gray-600 top-4 right-4 focus:outline-none'
           onClick={onCancel}
         >
           <IoClose />
@@ -16,8 +14,9 @@ const Modal = ({ children, onCancel = () => {} }) => {
         {children}
       </div>
     </div>
-    </>
   );
 };
 
+
 export default Modal;
+
