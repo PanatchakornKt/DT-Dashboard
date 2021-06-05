51f5bdfdf44cacb81d14ebf233a009e2081ce186
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-06-05 15:26:47 +0700

    install chess library

---
+++ components/game/chess.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as ChessJS from "chess.js";
+import { BehaviorSubject } from "rxjs";
+
+const Chess = typeof ChessJS === "function" ? ChessJS : ChessJS.Chess;
+const chess = new Chess();
+
+export const gameSubject = new BehaviorSubject({
+  board: chess.board(),
+});
